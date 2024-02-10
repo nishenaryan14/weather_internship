08a97b8cefba814875f840b2e8727ae23284dbc7
nishenaryan14 <aryannishen27@gmail.com>
2024-02-11 02:04:06 +0530

    line graph

--- src/components/lineGraph/LineGraph.jsx
+++ src/components/lineGraph/LineGraph.jsx
@@ -26,31 +26,31 @@ export const LineGraph = ({ city }) => {
         labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"], // Assuming day-wise labels
         datasets: [
           {
-            label: city[0].name,
+            label: city[0]?.name,
             data: city1,
             borderColor: "rgba(255, 99, 132, 1)",
             backgroundColor: "rgba(255, 99, 132, 0.2)",
           },
           {
-            label: city[1].name,
+            label: city[1]?.name,
             data: city2,
             borderColor: "rgba(54, 162, 235, 1)",
             backgroundColor: "rgba(54, 162, 235, 0.2)",
           },
           {
-            label: city[2].name,
+            label: city[2]?.name,
             data: city3,
             borderColor: "rgba(255, 206, 86, 1)",
             backgroundColor: "rgba(255, 206, 86, 0.2)",
           },
           {
-            label: city[3].name,
+            label: city[3]?.name,
             data: city4,
             borderColor: "rgba(75, 192, 192, 1)",
             backgroundColor: "rgba(75, 192, 192, 0.2)",
           },
           {
-            label: city[4].name,
+            label: city[4]?.name,
             data: city5,
             borderColor: "rgba(153, 102, 255, 1)",
             backgroundColor: "rgba(153, 102, 255, 0.2)",
